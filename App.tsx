632ddc755b48da0fd8109299f8df62e8a3326c5c
Anand Chowdhary <mail@anandchowdhary.com>
2019-07-19 00:08:11 +0530

    :sparkles: Store GitHub token in local storage

--- App.tsx
+++ App.tsx
@@ -26,33 +26,38 @@ interface NominatimResult {
     country_code: string;
   };
 }
-const track = async () => {
-  const { status } = await Permissions.askAsync(Permissions.LOCATION);
-  if (status !== "granted") alert("We couldn't get location permission!");
-  const location = await Location.getCurrentPositionAsync();
-  const reverseGeocoding = (await (await fetch(
-    `https://nominatim.openstreetmap.org/reverse?lat=${location.coords.latitude}&lon=${location.coords.longitude}&format=json`
-  )).json()) as NominatimResult;
-  const answer = {
-    altitude: location.coords.altitude,
-    accuracy: location.coords.accuracy,
-    heading: location.coords.heading,
-    speed: location.coords.speed,
-    latitude: reverseGeocoding.lat,
-    license: reverseGeocoding.license,
-    longitude: reverseGeocoding.lon,
-    ...reverseGeocoding.address
-  };
-};
 
-export default class App extends React.Component<{}, { token: string }> {
+export default class App extends React.Component<{}, { token: string, newToken: string }> {
   constructor(props: any) {
     super(props);
-    this.state = { token: "" };
+    this.state = { token: "", newToken: "" };
   }
   async componentDidMount() {
     const token = await AsyncStorage.getItem("token");
     this.setState({ token });
+  }
+  async track() {
+    if (!this.state.token) alert("We couldn't find a GitHub token!");
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") alert("We couldn't get location permission!");
+    const location = await Location.getCurrentPositionAsync();
+    const reverseGeocoding = (await (await fetch(
+      `https://nominatim.openstreetmap.org/reverse?lat=${location.coords.latitude}&lon=${location.coords.longitude}&format=json`
+    )).json()) as NominatimResult;
+    const answer = {
+      altitude: location.coords.altitude,
+      accuracy: location.coords.accuracy,
+      heading: location.coords.heading,
+      speed: location.coords.speed,
+      latitude: reverseGeocoding.lat,
+      license: reverseGeocoding.license,
+      longitude: reverseGeocoding.lon,
+      ...reverseGeocoding.address
+    };
+  }
+  async save() {
+    this.setState({ token: this.state.newToken });
+    AsyncStorage.setItem("token", this.state.newToken);
   }
   render() {
     return (
@@ -61,18 +66,20 @@ export default class App extends React.Component<{}, { token: string }> {
         <Text style={styles.text}>Track your current location</Text>
         {!!this.state.token ? (
           <View>
-            <Button title="Track" onPress={() => track()} />
+            <Button title="Track" onPress={() => this.track()} />
+            <Button title="Edit token" onPress={() => this.setState({ newToken: this.state.token || "", token: "" })} />
           </View>
         ) : (
           <View>
             <Text>No token found!</Text>
+            <TextInput
+              style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+              onChangeText={newToken => this.setState({ newToken })}
+              value={this.state.newToken}
+            />
+            <Button title="Save" onPress={() => this.save()} />
           </View>
         )}
-        <TextInput
-          editable={true}
-          onChangeText={text => console.log(text)}
-          value=""
-        />
       </View>
     );
   }
